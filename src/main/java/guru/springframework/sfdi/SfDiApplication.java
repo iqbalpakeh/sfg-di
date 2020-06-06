c43414b2f6ce01dae2600dcb621bc45d80e266d7
Progrema Studio <iqbalpakeh@gmail.com>
2020-06-06 08:37:34 +0700

    Implement manual and spring DI concept

--- src/main/java/guru/springframework/sfdi/SfDiApplication.java
+++ src/main/java/guru/springframework/sfdi/SfDiApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfdi;
 
+import guru.springframework.sfdi.controllers.ConstructorInjectedController;
 import guru.springframework.sfdi.controllers.MyController;
+import guru.springframework.sfdi.controllers.PropertyInjectedController;
+import guru.springframework.sfdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,10 +14,23 @@ public class SfDiApplication {
 	public static void main(String[] args) {
 
 		ApplicationContext ctx = SpringApplication.run(SfDiApplication.class, args);
-		MyController myController = (MyController) ctx.getBean("myController");
 
+		MyController myController = (MyController) ctx.getBean("myController");
 		String greeting = myController.sayHello();
 		System.out.println(greeting);
+
+		System.out.println("----- Property");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("----- Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("----- Constructor");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+
 	}
 
 }
--- src/main/java/guru/springframework/sfdi/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/sfdi/controllers/PropertyInjectedController.java
@@ -1,9 +1,13 @@
 package guru.springframework.sfdi.controllers;
 
 import guru.springframework.sfdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
 
+    @Autowired
     public GreetingService greetingService;
 
     public String getGreeting() {
